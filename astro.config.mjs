5797799c3d265ff9dba58794eadec86ba32e6bc4
Adam Davies <adavies@secondnature.com>
2025-12-28 12:43:09 -0600

    Update site and base path configuration to use environment variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://adamdavies1915.github.io',
-  base: '/alysWebsite/',
+  site: process.env.SITE_URL || 'https://alysmurray.com',
+  base: process.env.BASE_PATH || '/',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
